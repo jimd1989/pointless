fb8289ae611fd841c03e6dc1d125c54fd6113c12
Jim Dalrymple <jamesdalrymple@protonmail.com>
2020-07-14 23:41:49 +1000

    Making point-free macros more composable

--- pointless.scm
+++ pointless.scm
@@ -8,6 +8,38 @@
   (syntax-rules ()
     ((_ f ...) (lambda (α) (f ... α)))))
 
+; application (f α) = (f α)
+(define (application f α) (f α))
+
+; function composition (f g α) = (f (g α))
+(define-syntax delegate-delay
+  (syntax-rules (&&& ***)
+    ((_ (&&& α ...)) (fanout α ...))
+    ((_ (*** α ...)) (split-strong α ...))
+    ((_ (f α ...)) (delay f α ...))
+    ((_ f) (delay f))))
+
+(define-syntax delay-params
+  (syntax-rules ()
+    ((_ (f ...)) (list (delegate-delay (f ...))))
+    ((_ f) (list (delegate-delay f)))
+    ((_ (f ...) g ...) (cons (delegate-delay (f ...)) (delay-params g ...)))
+    ((_ f g ...) (cons (delegate-delay f) (delay-params g ...)))))
+
+(define-syntax composition
+  (syntax-rules ()
+    ((_ f ...) (lambda (α) (foldr application α (delay-params f ...))))))
+
+; fanout (f g α) = '((f α) (g α))
+(define-syntax fanout
+  (syntax-rules ()
+    ((_ f ...) (lambda (α) (map (lambda (ω) (ω α)) (delay-params f ...))))))
+
+; split strong (f g '(α ω)) = '((f α) (g ω))
+(define-syntax split-strong
+  (syntax-rules ()
+    ((_ f ...) (lambda (α) (map application (delay-params f ...) α)))))
+
 ; monadic hook (f g α) = (f α (g α))
 (define-syntax hook
   (syntax-rules ()
@@ -28,35 +60,11 @@
   (syntax-rules ()
     ((_ f g h ...) (lambda (α ω) (f (g α) (h ω) ...)))))
 
-; fanout (f g α) = '((f α) (g α))
-(define-syntax fanout
-  (syntax-rules ()
-    ((_ f ...) (lambda (α) (list (f α) ...)))))
-
 ; identity α = α
 (define (id α) α)
 
 ; const (α ω) = α
 (define (const α) (λ (ω) α))
-
-; application (f α) = (f α)
-(define (application f α) (f α))
-
-; split strong (f g '(α ω)) = '((f α) (g ω))
-(define (split-strong . fs)
-  (λ (α) (map application fs α)))
-
-; function composition (f g α) = (f (g α))
-(define-syntax composition-helper
-  (syntax-rules ()
-    ((_ (f ...)) (list (delay f ...)))
-    ((_ f) (list (delay f)))
-    ((_ (f ...) g ...) (cons (delay f ...) (composition-helper g ...)))
-    ((_ f g ...) (cons (delay f) (composition-helper g ...)))))
-
-(define-syntax composition
-  (syntax-rules ()
-    ((_ f ...) (lambda (α) (foldr application α (composition-helper f ...))))))
 
 ; uncurrying (f '(α ω) = (f α ω)
 (define (uncurry f) (λ (α) (apply f α)))
@@ -74,26 +82,27 @@
     ((_ α (p f) ...) (cond ((p α) (f α)) ...))))
 
 ; synonyms (poorly ordered functions will be flipped mercilessly)
-(define-syntax ⊃ (syntax-rules () ((_ . α) (delay . α))))       ;)C
-(define-syntax ∘ (syntax-rules () ((_ . α) (composition . α)))) ;Ob
-(define-syntax ◁ (syntax-rules () ((_ . α) (hook . α))))        ;Tl
-(define-syntax ◀ (syntax-rules () ((_ . α) (dyhook . α))))      ;PL
-(define-syntax ∴ (syntax-rules () ((_ . α) (fork . α))))        ;.:
-(define-syntax ∵ (syntax-rules () ((_ . α) (dyfork . α))))      ;:.
+(define-syntax ⊃ (syntax-rules () ((_ . α) (delay . α))))          ;)C
+(define-syntax ∘ (syntax-rules () ((_ . α) (composition . α))))    ;Ob
+(define-syntax ◁ (syntax-rules () ((_ . α) (hook . α))))           ;Tl
+(define-syntax ◀ (syntax-rules () ((_ . α) (dyhook . α))))         ;PL
+(define-syntax ∴ (syntax-rules () ((_ . α) (fork . α))))           ;.:
+(define-syntax ∵ (syntax-rules () ((_ . α) (dyfork . α))))         ;:.
 (define-syntax &&& (syntax-rules () ((_ . α) (fanout . α))))
-(define-syntax Λ (syntax-rules () ((_ . α) (cut . α))))         ;L*
-(define ⊥ id)                                                   ;-T
-(define ∞ const)                                                ;00
-(define ◇ append)                                               ;Dw
-(define ◆ conc)                                                 ;Db
+(define-syntax *** (syntax-rules () ((_ . α) (split-strong . α))))
+(define-syntax Λ (syntax-rules () ((_ . α) (cut . α))))            ;L*
+(define ⊥ id)                                                      ;-T
+(define ∞ const)                                                   ;00
+(define ◇ append)                                                  ;Dw
+(define ◆ conc)                                                    ;Db
 (define .◆ string-split)
 (define ◆. string-intersperse)
-(define ≡ equal?)                                               ;=3
-(define ∅ '())                                                  ;/0
-(define ↑ car)                                                  ;-!
+(define ≡ equal?)                                                  ;=3
+(define ∅ '())                                                     ;/0
+(define ↑ car)                                                     ;-!
 (define ↑↑ caar)
 (define ↑↑↑ caaar)
-(define ↓ cdr)                                                  ;-v
+(define ↓ cdr)                                                     ;-v
 (define ↓↓ cddr)
 (define ↓↓↓ cddr)
 (define ↓↑ cadr)
@@ -103,19 +112,18 @@
 (define ↓↑↑↑ caaadr)
 (define ↑. (flip take))
 (define ↓. (flip drop))
-(define ⊇ uncurry)                                              ;)_
-(define ⇒ map)                                                  ;=>
-(define ⇐ filter)                                               ;<=
-(define → foldr)                                                ;->
-(define ← foldl)                                                ;<-
-(define ∈ find)                                                 ;(-
-(define ∋ (flip member))                                        ;)-
-(define ∀ for-each)                                             ;FA
-(define ι iota)                                                 ;i*
-(define ρ length)                                               ;r*
-(define ⌐ flatten)                                              ;NI
-(define ⇔ reverse)                                              ;==
-(define *** split-strong)
+(define ⊇ uncurry)                                                 ;)_
+(define ⇒ map)                                                     ;=>
+(define ⇐ filter)                                                  ;<=
+(define → foldr)                                                   ;->
+(define ← foldl)                                                   ;<-
+(define ∈ find)                                                    ;(-
+(define ∋ (flip member))                                           ;)-
+(define ∀ for-each)                                                ;FA
+(define ι iota)                                                    ;i*
+(define ρ length)                                                  ;r*
+(define ⌐ flatten)                                                 ;NI
+(define ⇔ reverse)                                                 ;==
 (define $ apply)
 (define ~ flip)
 (define v⊥x vector->list)
